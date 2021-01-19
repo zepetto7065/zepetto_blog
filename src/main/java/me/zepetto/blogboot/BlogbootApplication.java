18171cdbc082b1731c26649b83d2c588dcf84830
smyoo <smyoo@ubivelox.com>
2021-01-19 16:11:16 +0900

    프로젝트 구성
    - 의존성 추가
    - 프로젝트 생성

---
+++ src/main/java/me/zepetto/blogboot/BlogbootApplication.java
@@ -0,0 +1,13 @@
+package me.zepetto.blogboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/zepetto/blogboot/BlogbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zepetto.blogboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
