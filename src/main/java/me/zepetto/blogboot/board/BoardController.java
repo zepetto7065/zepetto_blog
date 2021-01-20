0c451e642489c83b0cb1e5beee193430764515ec
smyoo <smyoo@ubivelox.com>
2021-01-20 17:58:36 +0900

    서버 데이터 전송
    - jquery-3.5.1 추가
    - 서버까지 데이터 전송확인

--- src/main/java/me/zepetto/blogboot/board/BoardController.java
+++ src/main/java/me/zepetto/blogboot/board/BoardController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,6 +35,14 @@ public class BoardController {
     @GetMapping("/form")
     public String createForm(Model m, Long bno){
         return "views/board/form";
+    }
+
+    @PostMapping("/create")
+    public String create(Board board){
+        System.out.println(board.getWriter());
+        System.out.println(board.getContent());
+
+        return "redirect:/";
     }
 
 }
--- src/main/java/me/zepetto/blogboot/config/WebConfig.java
+++ src/main/java/me/zepetto/blogboot/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
+        registry.addResourceHandler("/board/**")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod(60 * 60 * 24 * 365); //1년의 cache period
     }
