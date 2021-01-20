1a33971a8856c3fedf1574fdb23fc70922b53d58
smyoo <smyoo@ubivelox.com>
2021-01-20 10:38:59 +0900

    Index화면 구성
    - Index화면 생성
    - Index화면에 List 불러오기 및 DB 연동

--- src/main/java/me/zepetto/blogboot/HomeController.java
+++ src/main/java/me/zepetto/blogboot/HomeController.java
@@ -1,4 +1,21 @@
 package me.zepetto.blogboot;
 
+import me.zepetto.blogboot.board.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    BoardService boardService;
+
+    @GetMapping("/")
+    public String index(Model m){
+        m.addAttribute("board", boardService.getList());
+        return "views/index";
+    }
+
 }
--- src/main/java/me/zepetto/blogboot/board/BoardController.java
+++ src/main/java/me/zepetto/blogboot/board/BoardController.java
@@ -5,14 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
     @Autowired
     BoardService boardService;
+
 
     @GetMapping("/blog")
     public String blog(){
@@ -22,13 +21,6 @@ public class BoardController {
     @GetMapping("/post")
     public String post(){
         return "views/board/form";
-    }
-
-    @GetMapping("/hello")
-    public String index(){
-        final List<Board> list = boardService.getList();
-        System.out.println(list);
-        return "hello";
     }
 
 }
